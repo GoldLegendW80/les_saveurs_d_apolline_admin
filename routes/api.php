ce1fb0da5937b912f20f7cb761db2acb72e6c724
GoldLegendW80 <hthautin@gmail.com>
2024-10-07 13:17:11 +0200

    ok

---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\MenuController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/menus/all', [MenuController::class, 'getAllMenus'])->name('menus.getAllMenus');
